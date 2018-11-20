6eddf7d6e81a229979227596680658c6bd943823
masol <masol.li@gmail.com>
2018-11-21 04:02:05 +0800

    改进wwclass测试用例

--- test/spec/wwclass.js
+++ test/spec/wwclass.js
@@ -20,8 +20,20 @@ describe('wwclass元素机制', function () {
 
   it('类定义中，依赖成功不回调，失败可以回调类方法', function (done) {
     let sig = 0
+    let sig2 = 0
+    let fullfillTest = false; let fullFileTest2 = false
+    const allFullfilled = () => {
+      if (fullFileTest2 && fullfillTest) {
+        done()
+      }
+    }
     let deperr = function (errArray) {
-      sig++
+      if ($.isArray(errArray) && errArray.length === 1 && errArray[0].endsWith(notExistBase)) {
+        sig2++
+        chai.expect(sig2).to.be.equal(1, `类加载错误的通知有问题？`)
+        fullFileTest2 = true
+        allFullfilled()
+      }
     }
     // 类依赖失败的回调不能调用实例方法，而静态方法类未定义，只能前置定义失败方法。
     @wwjs.wwclass.dep([existFile], deperr)
@@ -41,15 +53,38 @@ describe('wwclass元素机制', function () {
         if ($.isArray(err) && err.length === 1 && err[0].endsWith(notExistBase)) {
           sig++
           chai.expect(sig).to.be.equal(1, `加载成功，也调用了错误回调？`)
-          done()
+          fullfillTest = true
+          allFullfilled()
         }
       }
     }
+
+    @wwjs.wwclass.dep([notExistFile], deperr)
+    class Test2 extends wwjs.wwclass {
+    }
     wwjs.wwclass.reg(Test, 'Test')
+    wwjs.wwclass.reg(Test2, 'Test2')
 
     setTimeout(() => {
-      wwjs.ui.$container().append(`<div id="wwtest1" data-wwclass="Test"></div>`)
+      wwjs.ui.$container().append(`<div id="wwtest1" data-wwclass="Test"></div><div id="wwtest2" data-wwclass="Test2"></div>`)
       // t0 = performance.now()
     }, 0)
+  })
+
+  it('依赖函数返回promise，并正确解析', function () {
+  })
+  it('同时创建多个元素只会加载一次,创建多个实例', function () {
+  })
+  it('watch可以正确接收属性改变事件，选项触发render', function () {
+  })
+  it('watch只在属性变化时才触发', function () {
+  })
+  it('watch可以正确接收子元素加入/删除事件，选项触发render', function () {
+  })
+  it('render方法利用的模板是增量更新', function () {
+  })
+  it('基类attr方法正确更新绑定的变量', function () {
+  })
+  it('析构函数触发', function () {
   })
 })
