452c6f121c541b929c7addce9234a560a1e3d49b
masol <masol.li@gmail.com>
2018-12-17 21:02:24 +0800

    完善build版的测试，依然使用jquery,而不是cash-dom,小bug处理起来痛苦。

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = env => {
       // symlinks: false,
       modules: [resolve('src'), resolve('node_modules')],
       alias: {
-        jquery: 'cash-dom',
+        // jquery: 'cash-dom',
         knockout: 'knockout/build/output/knockout-latest'
       }
     }
