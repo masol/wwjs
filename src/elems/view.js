c9fc646e9218cb0a2bdb83bd1907531850556636
masol <masol.li@gmail.com>
2018-12-22 03:45:41 +0800

    BREAK CHANGE:view中规避`data-forcelocal`的方法改为a链接中明确指定`target`属性.

--- src/elems/view.js
+++ src/elems/view.js
@@ -41,7 +41,7 @@ window.wwimport('ready', () => {
   @class View
   @classdesc View支持如下几个属性:
   - src: 指定View获取的url,如果src变化，则立即尝试获取数据．如果服务器组装，请不要设置src值，直接向view中填写内容.
-  - data-forcelocal: 指定view中的a[href]的链接，强制刷新本view．a[href]中也可以通过设置属性`data-refview`为false来强制链接点击不是刷新view.
+  - data-forcelocal: 指定view中的a[href]的链接，强制刷新本view．a[href]中也可以通过明确设置`target`来规避刷新view.
   @extends wwjs.wwclass
   */
   class View extends wwjs.wwclass {
@@ -59,8 +59,8 @@ window.wwimport('ready', () => {
       self.$ele.on('click', 'a[href]', function (e) {
         var $evt = $(this)
         var forcelocal = (self.$ele.attr('data-forcelocal') === 'true')
-        var refview = $evt.attr('data-refview')
-        if (refview === 'true' || (forcelocal && refview !== 'false')) {
+        var refview = $evt.attr('target')
+        if (!refview && forcelocal) {
           event.preventDefault()
           // set view url from $evt.attr("href");
           self.props.src = $evt.attr('href')
